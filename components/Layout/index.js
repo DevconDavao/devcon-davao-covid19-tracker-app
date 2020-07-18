452e8a426bd39a12a26c57b96dbffc1ce064eaf0
cdcdianne <cdcdianne@gmail.com>
2020-07-18 14:57:19 +0800

    Created index file for components

---
+++ components/Layout/index.js
@@ -0,0 +1,12 @@
+import Card from "./Card/Card";
+import Nav from "./Nav/Nav";
+import Footer from "./Footer/Footer";
+import Carousel from "./Carousel/Carousel";
+
+
+export {
+    Card,
+    Nav,
+    Footer,
+    Carousel,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import Nav from '../components/Layout/Nav/Nav';
-import Footer from '../components/Layout/Footer/Footer';
-import Carousel from '../components/Layout/Carousel/Carousel';
-import Card from '../components/Layout/Card/Card';
+import { Card, Nav, Footer, Carousel } from '../components/Layout';
+
 import './index.css'
 
 export default function Home() {
@@ -21,39 +19,39 @@ export default function Home() {
       total_deaths: 5,
       new_deaths: 2
     },
-    {
-      hospital: 'Southern Philippine Medical Center',
-      total_cases: 1000,
-      new_cases: 13,
-      total_recoveries: 150,
-      new_recoveries: 50,
-      total_suspects: 50,
-      new_suspects: 25,
-      active_cases: 50,
-      new_active_cases: 10,
-      total_deaths: 5,
-      new_deaths: 2
-    },
-    {
-      hospital: 'Southern Philippine Medical Center',
-      total_cases: 1000,
-      new_cases: 13,
-      total_recoveries: 150,
-      new_recoveries: 50,
-      total_suspects: 50,
-      new_suspects: 25,
-      active_cases: 50,
-      new_active_cases: 10,
-      total_deaths: 5,
-      new_deaths: 2
-    }
+    // {
+    //   hospital: 'Southern Philippine Medical Center',
+    //   total_cases: 1000,
+    //   new_cases: 13,
+    //   total_recoveries: 150,
+    //   new_recoveries: 50,
+    //   total_suspects: 50,
+    //   new_suspects: 25,
+    //   active_cases: 50,
+    //   new_active_cases: 10,
+    //   total_deaths: 5,
+    //   new_deaths: 2
+    // },
+    // {
+    //   hospital: 'Southern Philippine Medical Center',
+    //   total_cases: 1000,
+    //   new_cases: 13,
+    //   total_recoveries: 150,
+    //   new_recoveries: 50,
+    //   total_suspects: 50,
+    //   new_suspects: 25,
+    //   active_cases: 50,
+    //   new_active_cases: 10,
+    //   total_deaths: 5,
+    //   new_deaths: 2
+    // }
   ]
 
   return (
     <div className="container">
       <Head>
         <title>Covid19-Tracker</title>
-        <link rel="icon" href="/devcon.png" />
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <Nav />
